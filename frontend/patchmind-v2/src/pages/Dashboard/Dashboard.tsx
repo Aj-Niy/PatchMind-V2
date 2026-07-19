07162e24092c2f9c2e6285e8bd081da0a556146a
Ajit Pandey <ajitpandey4572@gmail.com>
2026-07-20 00:38:55 +0530

    Setup React, TypeScript and Tailwind foundation

---
+++ frontend/patchmind-v2/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-blue-500">
+        Dashboard
+      </h1>
+    </div>
+  )
+}
---
+++ frontend/patchmind-v2/src/pages/History/History.tsx
@@ -0,0 +1,12 @@
+
+function History() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold text-blue-500">
+        History
+      </h1>
+    </div>
+  )
+}
+
+export default History
